bad4d1f9f99eeee56a5caf193c7538a3d828065c
bijan <bijan@localhost>
2022-04-18 10:20:40 -0600

    Let gups use huge pages

--- bmks/gups/gups.c
+++ bmks/gups/gups.c
@@ -151,7 +151,7 @@ int main(int argc, char **argv)
   fprintf(stderr, "field of 2^%lu (%lu) bytes\n", expt, size);
   fprintf(stderr, "%ld byte element size (%ld elements total)\n", elt_size, size / elt_size);
 
-  p = mmap(ADDRESS, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+  p = mmap(ADDRESS, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
   if (p == MAP_FAILED) {
     perror("mmap");
     assert(0);
