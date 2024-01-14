cd9ea6808f80a44b84dfde46cc08e4188ff204bf
Bijan Tabatabai <bijan311@gmail.com>
2024-01-14 15:21:17 -0600

    Add plot-stream-resuls.py

---
+++ scripts/plot-stream-results.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+filename = sys.argv[1]
+
+configs = []
+throughputs = {
+    "Copy": [],
+    "Scale": [],
+    "Add": [],
+    "Triad": []
+}
+
+# Read the data
+with open(filename, "r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        configs.append(row['Type'])
+        throughputs['Copy'].append(float(row['Copy']))
+        throughputs['Add'].append(float(row['Add']))
+        throughputs['Scale'].append(float(row['Scale']))
+        throughputs['Triad'].append(float(row['Triad']))
+
+x = np.arange(len(configs))
+print(x)
+width = 0.2
+multiplier = 0
+
+plt.figure(figsize=(10, 6))
+for attr, measurements in throughputs.items():
+    offset = width * multiplier
+    plt.bar(x + offset, measurements, width, label=attr)
+    multiplier += 1
+
+plt.title("Bandwidth Reported By Stream With BWFSMM", fontsize=24)
+plt.xticks(x + (1.5 * width), configs, fontsize=18, rotation=0)
+plt.yticks(fontsize=18)
+plt.ylabel("Bandwidth (MB/s)", fontsize=22)
+plt.legend()
+
+plt.show()
